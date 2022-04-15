b4f8e7dad2b11ca1ef377730a1185a35babc27d9
lyx-jay <1498781418@qq.com>
2022-04-15 16:26:36 +0800

    add 受控组件

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from './context/App';
 // import App from './hoc/HighComponent';
-import App from './component_other/ComponentOther';
+// import App from './component_other/ComponentOther';
+// import App from './controled_components/controed_components';
+import App from './controled_components/uncontroled_components';
 
 
 ReactDOM.render(
